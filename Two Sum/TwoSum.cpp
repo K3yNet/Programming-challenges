0bc745640308f38a5081f72cb0efb753d01ed9b7
K3yNet <enzo_vel@hotmail.com>
2024-04-15 02:51:22 -0300

    Update TwoSum.cpp

---
+++ Two Sum/TwoSum.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+int target = 7;
+vector<int> nums = {2, 7, 11, 5, 8, 15, 3};
+unordered_map<int, int> hasher;
+
+int resultX, resultY;
+
+int main(){
+    for (unsigned i = 0; i < nums.size(); i++) {
+        if (hasher.find(nums[i]) != hasher.end()) {
+            resultX = hasher[nums[i]];
+            resultY = i;
+        }
+        hasher[target - nums[i]] = i;
+    }
+    cout << resultX << " " << resultY << endl;
+}
