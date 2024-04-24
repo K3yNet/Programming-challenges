3086f3e7bf58ff52cadc36c13b56745818d3aff0
K3yNet <enzo_vel@hotmail.com>
2024-04-24 18:29:45 -0300

    matrixRotation Update

--- Matrix Rotation/matrixRotation.cpp
+++ Matrix Rotation/matrixRotation.cpp
@@ -1,47 +1,32 @@
 #include <iostream>
-
-#define N 10
-
+#include <vector>
 using namespace std;
 
-int** criarMatriz(){
-    int** mtz = new int*[N];
-    for(int i=0; i<N; i++){
-        mtz[i] = new int[N];
-        for(int j=0; j<N; j++){
-            mtz[i][j] = N*i + j + 1;
-        }
-    }
-    return mtz;
-}
+vector<vector<int>> matrix = {
+        {1,  2,  3,  4},
+        {5,  6,  7,  8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+const int N = matrix.size();
 
 int main(){
-    int** mtz = criarMatriz();
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++)
-            cout << mtz[i][j] << "\t";
-        cout << endl;
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = i; j < N - i - 1; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[N - 1 - j][i];
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j];
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i];
+            matrix[j][N - 1 - i] = temp;
+        }
     }
-    cout << "--------------------------------------" << endl;
 
-    for(int i=0; i<N; i++)
-        for(int j=i+1; j<N; j++)
-            swap(mtz[i][j], mtz[j][i]);
-    
-    for(int i=0; i<N; i++)
-        for(int j=0; j<N/2; j++)
-            swap(mtz[i][j], mtz[i][N-j-1]);
-
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++)
-            cout << mtz[i][j] << "\t";
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << matrix[i][j] << " ";
         cout << endl;
     }
 
-    for(int i=0; i<N; i++)
-        delete [] mtz[i];
-    delete [] mtz;
-
-    return 0;
 }
